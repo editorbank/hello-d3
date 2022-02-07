f53a9e566f99cf2ce7bf50bff27ecced1e1c8628
editorbank <>
2022-02-07 23:32:08 +0300

    formatted rotated text

--- src/draw.ts
+++ src/draw.ts
@@ -10,7 +10,7 @@ export function crop_string(s :string, maxLen :number = 0) :string {
 
 export function draw(elementSelector :string, in_data :InData){
   var svg = d3.select(elementSelector),
-  margin = {top: 10, right: 0, bottom: 50, left: 80},
+  margin = {top: 10, right: 0, bottom: 90, left: 90},
   width = +svg.attr("width") - margin.left - margin.right,
   height = +svg.attr("height") - margin.top - margin.bottom;
 
@@ -30,26 +30,34 @@ export function draw(elementSelector :string, in_data :InData){
     .attr("class", "axis axis--x")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x))
+     .selectAll("text")
+     .attr("transform", "translate(-20,0)rotate(-45)")
+    //  .style("alignment-baseline", "after-edge")
+    .style("alignment-baseline", "central")
+    // .style("alignment-baseline", "middle")
+    // .style("alignment-baseline", "before-edge")
+    .style("text-anchor", "end")
+    .style("font-size", '16px')
+    .style("fill", "black")
   ;
 
   g.append("g")
     .attr("class", "axis axis--y")
-    .call(d3.axisLeft(y).ticks(10))
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("y", ".2em")
-    .attr("text-anchor", "end")
-    .text("Frequency")
+    .call(d3.axisLeft(y).ticks(5))
+    .selectAll("text")
+    .attr("transform", "translate(-0,0)rotate(-45)")
+    .style("text-anchor", "end")
+    .style("font-size", '16px')
+    .style("fill", "black")
   ;
 
   g.selectAll(".bar")
     .data(in_data.labels)
     .enter().append("rect")
-    .attr("class", "bar")
     .attr("x", d => x(d))
     .data(in_data.values)
     .attr("y", d => y(d))
+    .attr("class", v => v < in_data.overValue ? "bar" : "barover")
     .attr("width", x.bandwidth())
     .attr("height", function(d) { return height - y(d); })
   ;
