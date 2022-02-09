faea54108a20a0bca6214d6f7c8c2e8e98ac5f2c
editorbank <>
2022-02-09 23:10:16 +0300

    add grid

--- src/draw.ts
+++ src/draw.ts
@@ -1,43 +1,64 @@
 import { InData } from "./in_data";
 import * as d3 from "d3";
 
-export function truncate_str(s :string, maxLen :number = 0) :string {
-  if(s.length > maxLen && maxLen !== 0){
+export function truncate_str(s: string, maxLen: number = 0): string {
+  if (s.length > maxLen && maxLen !== 0) {
     var end = '…';
-    return s.substring(0,maxLen - end.length) + end;
+    return s.substring(0, maxLen - end.length) + end;
   } else {
     return s;
   }
 }
 
-export function draw(elementSelector :string, in_data :InData){
+export function draw(elementSelector: string, in_data: InData) {
   var svg = d3.select(elementSelector),
-  margin = {top: 10, right: 0, bottom: 90, left: 90},
-  width = +svg.attr("width") - margin.left - margin.right,
-  height = +svg.attr("height") - margin.top - margin.bottom;
+    margin = { top: 10, right: 0, bottom: 90, left: 90 },
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom;
 
-  in_data.labels = in_data.labels.map( s => truncate_str(s,10));
+  in_data.labels = in_data.labels.map(s => truncate_str(s, 10));
   var x = d3.scaleBand().rangeRound([0, width]).padding(0.1);
   x.domain(in_data.labels);
 
-  var maxY = d3.max(in_data.values/*, d=>d*/); 
+  var valRange = d3.extent(in_data.values);
+  var minY = in_data.bottomValue || in_data.bottomValue === 0 ? Math.min(in_data.bottomValue, valRange[0]) : valRange[0];
+  var maxY = in_data.topValue || in_data.topValue === 0 ? Math.max(in_data.topValue, valRange[1]) : valRange[1];
+
+  var ovrY = 'string' === typeof in_data.overValue
+    ? {
+      "max": maxY,
+      "average": d3.mean(in_data.values),
+      "mode": d3.mode(in_data.values),
+      "median": d3.median(in_data.values),
+    }[in_data.overValue] || parseFloat(in_data.overValue) || undefined
+    : in_data.overValue;
   var y = d3.scaleLinear()
     .rangeRound([height, 0])
-    .domain([0, maxY ])
-  ;
+    .domain([minY, maxY])
+    .nice()
+    ;
   
   var g = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-  ;
+    ;
 
-  if (in_data.overValue || in_data.overValue === 0)
+  if ('number' === typeof (ovrY))
     svg.append("path")
-      .attr("d","M0,0l"+width+",0")
-      .attr('stroke','red')
-      .attr('stroke-width','.5')
-      .attr('stroke-dasharray','10,5')
-      .attr('transform', `translate(${margin.left},${margin.top+y(in_data.overValue)})`)
-  ;
+      .attr("d", "M0,0l" + width + ",0")
+      .attr('stroke', 'red')
+      .attr('stroke-width', '1')
+      .attr('stroke-dasharray', '10,5')
+      .attr('transform', `translate(${margin.left},${margin.top + y(ovrY)})`)
+      ;
+
+  var ticks = 5;    
+  svg.selectAll().data(y.ticks(ticks)).enter()
+    .append('path')
+    .attr("d", 'M0,0l' + width + ',0')
+    .attr('stroke', 'rgba(0,0,0,.5)')
+    .attr('stroke-width', '.5')
+    .attr('stroke-dasharray', '3,5')
+    .attr("transform", d => "translate(" + margin.left + "," + (margin.top + y(d)) + ")")
 
   g.append("g")
     .attr("class", "axis axis--x")
@@ -49,17 +70,17 @@ export function draw(elementSelector :string, in_data :InData){
     .style("text-anchor", "end")
     .style("font-size", '16px')
     .style("fill", "black")
-  ;
+    ;
 
   g.append("g")
     .attr("class", "axis axis--y")
-    .call(d3.axisLeft(y).ticks(5))
+    .call(d3.axisLeft(y).ticks(ticks))
     .selectAll("text")
-    .attr("transform", "translate(-0,0)rotate(-45)")
+    .attr("transform", "rotate(-45)")
     .style("text-anchor", "end")
     .style("font-size", '16px')
     .style("fill", "black")
-  ;
+    ;
 
   g.selectAll(".bar")
     .data(in_data.labels)
@@ -67,9 +88,10 @@ export function draw(elementSelector :string, in_data :InData){
     .attr("x", d => x(d))
     .data(in_data.values)
     .attr("y", d => y(d))
-    .attr("class", v => v < in_data.overValue ? "bar" : "barover")
+    .attr("class", 'number' === typeof ovrY ? (v => v >= ovrY ? "barover" : "bar") : "bar")
     .attr("width", x.bandwidth())
-    .attr("height", function(d) { return height - y(d); })
-  ;
+    .attr("height", function (d) { return height - y(d); })
+    ;
+
 
 }
--- src/in_data.ts
+++ src/in_data.ts
@@ -1,13 +1,15 @@
 export type HTMLColor = string | CanvasGradient | CanvasPattern; 
 
 export interface InData {
-  color :'steelblue' | HTMLColor ;
-  labels :string[];
-  title ?:string;
-  values :number[];
-  overValue :number;
-  overColor :'red' | HTMLColor;
-  overLineColor :'red' | HTMLColor;
+  color: 'steelblue' | HTMLColor ;
+  labels: string[];
+  title?: string;
+  values: number[];
+  bottomValue?: number;
+  overValue?: number | 'average' | 'max' | 'mode' | 'median';
+  topValue?: number;
+  overColor: 'brown' | HTMLColor;
+  overLineColor: 'red' | HTMLColor;
   maxLabelLen?: 0 | number;
   verticalGridColor?: HTMLColor;
   fontName?: string;  
@@ -20,7 +22,11 @@ var in_data :InData = {
   labels: [ "США", "Индонезия (Очень длинное название)", "Китай", "Индия", "Бразилия", "Пакистан", "Нигерия", "Бангладеш", "Россия", "Япония"],
   title: 'Население',
   values: [326625791, 260580739, 1379302771, 1281935911, 207353391, 204924861, 190632261, 157826578, 142257519, 126451398],
+  // topValue: 2_000_000_000,
   overValue: 400000000,
+  // overValue: 'average',
+  // overValue: 'median',
+  //  bottomValue: 0,
   overColor: 'brown',
   overLineColor: 'red',
   maxLabelLen: 15,
